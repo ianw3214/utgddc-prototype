6127a2cabca8c557b48bcb4efdf51d87ac87a9ba
joshmartine <joshhmartine@gmail.com>
2019-10-17 18:16:34 -0400

    Temporary fix

--- page.js
+++ page.js
@@ -2,7 +2,7 @@ showdown.setFlavor('github');
 
 $(function() {
     var converter = new showdown.Converter();
-    $.get($("#page-script").attr("name"), function(data) {
+    $.get("https://raw.githubusercontent.com/ianw3214/utgddc-prototype/master/" + $("#page-script").attr("name"), function(data) {
         $('#desc').html(converter.makeHtml(data));
     });
 })
